717a154b0ec016a2c546f56e1ad5bd840b8953d3
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-02-13 14:40:43 +0530

    Added db.connect

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './typedefs/index';
+import { connect } from './db'; 
 
 const server = new ApolloServer({ typeDefs });
 
 async function startServer() {
-  await server.start(); // Make sure to await server.start() before calling server.applyMiddleware()
+  await connect();  
+  
+  await server.start();  
 
   const app = express();
   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4000;
 
   app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}${server.graphqlPath}`);
